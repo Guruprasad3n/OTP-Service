52a8435906cdbba3d1c4a9659b0320314f689908
Virendra kumar dwivedi <101566111+Virendrakumardwivedi@users.noreply.github.com>
2023-11-22 18:27:24 +0530

    test cases-code added

--- apps/server/src/__tests__/cleanupUtil.test.ts
+++ apps/server/src/__tests__/cleanupUtil.test.ts
@@ -1,1 +1,30 @@
 // __tests__/cleanupUtil.test.ts
+import cleanupDatabase from '../utils/cleanupUtil';
+import OtpModel from '../models/otpModel';
+
+jest.mock('../models/otpModel');
+
+describe('cleanupDatabase', () => {
+  it('should clean up expired records successfully', async () => {
+    OtpModel.updateMany = jest.fn().mockResolvedValue({ nModified: 5 });
+
+    await cleanupDatabase();
+
+    expect(OtpModel.updateMany).toHaveBeenCalledWith(
+      { 'passwords.expiresAt': { $lt: expect.any(Date) } },
+      { $pull: { passwords: { expiresAt: { $lt: expect.any(Date) } } } },
+      { multi: true }
+    );
+  });
+
+  it('should handle errors during cleanup', async () => {
+    OtpModel.updateMany = jest.fn().mockRejectedValue(new Error('Cleanup error'));
+
+    await cleanupDatabase();
+
+    // Write assertions based on your error handling mechanism
+  });
+
+  // Add more test cases for other scenarios
+});
+
--- apps/server/src/__tests__/otpLimitMiddleware.test.ts
+++ apps/server/src/__tests__/otpLimitMiddleware.test.ts
@@ -1,1 +1,67 @@
 // __tests__/otpLimitMiddleware.test.ts
+
+import { Request, Response, NextFunction } from "express";
+import otpLimitMiddleware from "../middlewares/otpLimitMiddleware";
+// import rateLimit from "express-rate-limit";
+import rateLimit, { RateLimitRequestHandler, Options, ClientRateLimitInfo } from "express-rate-limit";
+
+jest.mock("express-rate-limit");
+
+
+// const mockedRateLimit = rateLimit as jest.MockedFunction<
+//   (passedOptions?: Partial<Options>) => RateLimitRequestHandler
+// >;
+const mockedRateLimit = rateLimit as jest.MockedFunction<any>;
+
+// const mockedRateLimit = rateLimit as jest.MockedFunction<
+//   (options?: Partial<Options>) => RateLimitRequestHandler & {
+//     resetKey: (key: string) => void;
+//     getKey: (key: string) => ClientRateLimitInfo | Promise<ClientRateLimitInfo>;
+//   }
+// >;
+describe("otpLimitMiddleware", () => {
+  it("should call rate limit middleware and pass to the next middleware", () => {
+    const req = {} as Request;
+    const res = {} as Response;
+    const next = jest.fn() as NextFunction;
+
+    otpLimitMiddleware(req, res, next);
+
+    expect(rateLimit).toHaveBeenCalled();
+    expect(next).toHaveBeenCalled();
+  });
+
+  it("should handle rate limit exceeded", () => {
+    const req = {} as Request;
+    const res = { status: jest.fn(), send: jest.fn() } as unknown as Response;
+    const next = jest.fn() as NextFunction;
+
+    // Simulate rate limit exceeded scenario
+    // rateLimit.mockImplementation(
+    //   () => (req: Request, res: Response, next: NextFunction) => {
+    //     res.status(429).send({ success: false, error: "Rate limit exceeded" });
+    //   }
+    // );
+
+    mockedRateLimit.mockImplementation((options?: Partial<Options>) => {
+        return (req: Request, res: Response, next: NextFunction) => {
+          res.status(429).send({ success: false, error: 'Rate limit exceeded' });
+        };
+      });
+
+    // mockedRateLimit.mockImplementation((options?: Partial<Options>) => {
+    //     return (req: Request, res: Response, next: NextFunction) => {
+    //       res.status(429).send({ success: false, error: 'Rate limit exceeded' });
+    //     };
+    //   });
+    otpLimitMiddleware(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(429);
+    expect(res.send).toHaveBeenCalledWith(
+      expect.objectContaining({ success: false })
+    );
+    expect(next).not.toHaveBeenCalled();
+  });
+
+  // Add more test cases for other scenarios
+});
